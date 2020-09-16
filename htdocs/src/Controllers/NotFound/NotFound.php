9fbb14a521a2e30eec3d9bd541429df99a3f5697
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-09-16 14:09:23 +0200

    Update NotFound controller

--- htdocs/src/Controllers/NotFound/NotFound.php
+++ htdocs/src/Controllers/NotFound/NotFound.php
@@ -2,16 +2,27 @@
 /**
  * src/Controllers/NotFound.php
  */
-require_once(__DIR__ . '/../Core/Controllers/Controller.php');
-require_once(__DIR__ . '/../Core/Controllers/InvocableInterface.php');
+require_once(__DIR__ . '/../../Core/Controllers/Controller.php');
+require_once(__DIR__ . '/../../Core/Controllers/InvocableInterface.php');
+require_once(__DIR__ . '/../../Core/Http/Response/Response.php');
+require_once(__DIR__ . '/../../Core/Http/Response/HtmlResponse.php');
+require_once(__DIR__ . '/../Menu/MenuController.php');
 
 class NotFound extends Controller implements InvocableInterface {
 
     public function __construct() {
         $this->view = __DIR__ . '/Views/notfound.view.php';
+
+        // Instancier les contrôleurs "globaux" : menu, footer, etc...
+        $this->globalControllers = [
+            'menu' => new MenuController()
+        ];
     }
 
     public function notFound() {
-        $this->renderView();
+        $this->response = new HtmlResponse($this);
+
+        // Retourne l'objet Response
+        return $this->response;
     }
 }
